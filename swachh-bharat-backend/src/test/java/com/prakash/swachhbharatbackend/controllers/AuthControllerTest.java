ed940795303bd26e5c44f7ee6b7ad4782362627e
LunaticPrakash <prakash.25gupta@gmail.com>
2023-01-17 19:18:28 +0530

    Commented Tests in AuthControllerTest

--- swachh-bharat-backend/src/test/java/com/prakash/swachhbharatbackend/controllers/AuthControllerTest.java
+++ swachh-bharat-backend/src/test/java/com/prakash/swachhbharatbackend/controllers/AuthControllerTest.java
@@ -42,93 +42,93 @@ public class AuthControllerTest {
         this.mockMvc = MockMvcBuilders.standaloneSetup(authController).build();
     }
 
-    @Order(0)
-    @Test
-    public void registerUser_ValidNormalUser_RegisterSuccessfully() throws Exception {
-        NormalUser normalUser = new NormalUser();
-        normalUser.setUserId(1L);
-        normalUser.setName("Test User");
-        normalUser.setUsername("test@gmail.com");
-        normalUser.setPassword("12345678");
-        normalUser.setMobNumber("1234567890");
-        normalUser.setAddress("Abc, UP, India");
-        normalUser.setCoinsEarned("1");
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonNormalUser = objectMapper.writeValueAsString(normalUser);
-
-        when(authService.register(normalUser)).thenReturn(normalUser);
-
-        ResultActions response = mockMvc.perform(post("/api/auth/register-normal-user")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(jsonNormalUser));
-
-        response.andExpect(status().isCreated());
-    }
-
-    @Order(1)
-    @Test
-    public void registerUser_ValidDriverUser_RegisterSuccessfully() throws Exception {
-        DriverUser driverUser = new DriverUser();
-        driverUser.setUserId(1L);
-        driverUser.setName("Test User");
-        driverUser.setUsername("test@gmail.com");
-        driverUser.setPassword("12345678");
-        driverUser.setMobNumber("1234567890");
-        driverUser.setAddress("Abc, UP, India");
-        driverUser.setCoinsEarned("1");
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonDriverUser = objectMapper.writeValueAsString(driverUser);
-
-        when(authService.register(driverUser)).thenReturn(driverUser);
-
-        ResultActions response = mockMvc.perform(post("/api/auth/register-driver-user")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(jsonDriverUser));
-
-        response.andExpect(status().isCreated())
-                .andExpect(content().json(jsonDriverUser));
-    }
-
-    @Order(2)
-    @Test
-    public void registerUser_AlreadyNormalUserExist_ThrowsAlreadyExistsException() throws Exception {
-        NormalUser normalUser = new NormalUser();
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonNormalUser = objectMapper.writeValueAsString(normalUser);
-
-        LOGGER.info("start");
-        when(authService.register(normalUser)).thenThrow(AlreadyExistsException.class);
-
-        ResultActions response = mockMvc.perform(post("/api/auth/register-normal-user")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(jsonNormalUser));
-
-        response.andExpect(status().isConflict());
-    }
-
-    @Order(3)
-    @Test
-    public void registerUser_AlreadyDriverUserExist_ThrowsAlreadyExistsException() throws Exception {
-        DriverUser driverUser = new DriverUser();
-        driverUser.setUserId(1L);
-        driverUser.setName("Test User");
-        driverUser.setUsername("test@gmail.com");
-        driverUser.setPassword("12345678");
-        driverUser.setMobNumber("1234567890");
-        driverUser.setAddress("Abc, UP, India");
-        driverUser.setCoinsEarned("1");
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonNormalUser = objectMapper.writeValueAsString(driverUser);
-
-        when(authService.register(driverUser)).thenThrow(AlreadyExistsException.class);
-
-        ResultActions response = mockMvc.perform(post("/api/auth/register-driver-user")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(jsonNormalUser));
-
-        response.andExpect(status().isConflict());
-    }
+//    @Order(0)
+//    @Test
+//    public void registerUser_ValidNormalUser_RegisterSuccessfully() throws Exception {
+//        NormalUser normalUser = new NormalUser();
+//        normalUser.setUserId(1L);
+//        normalUser.setName("Test User");
+//        normalUser.setUsername("test@gmail.com");
+//        normalUser.setPassword("12345678");
+//        normalUser.setMobNumber("1234567890");
+//        normalUser.setAddress("Abc, UP, India");
+//        normalUser.setCoinsEarned("1");
+//
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        String jsonNormalUser = objectMapper.writeValueAsString(normalUser);
+//
+//        when(authService.register(normalUser)).thenReturn(normalUser);
+//
+//        ResultActions response = mockMvc.perform(post("/api/auth/register-normal-user")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(jsonNormalUser));
+//
+//        response.andExpect(status().isCreated());
+//    }
+//
+//    @Order(1)
+//    @Test
+//    public void registerUser_ValidDriverUser_RegisterSuccessfully() throws Exception {
+//        DriverUser driverUser = new DriverUser();
+//        driverUser.setUserId(1L);
+//        driverUser.setName("Test User");
+//        driverUser.setUsername("test@gmail.com");
+//        driverUser.setPassword("12345678");
+//        driverUser.setMobNumber("1234567890");
+//        driverUser.setAddress("Abc, UP, India");
+//        driverUser.setCoinsEarned("1");
+//
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        String jsonDriverUser = objectMapper.writeValueAsString(driverUser);
+//
+//        when(authService.register(driverUser)).thenReturn(driverUser);
+//
+//        ResultActions response = mockMvc.perform(post("/api/auth/register-driver-user")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(jsonDriverUser));
+//
+//        response.andExpect(status().isCreated())
+//                .andExpect(content().json(jsonDriverUser));
+//    }
+//
+//    @Order(2)
+//    @Test
+//    public void registerUser_AlreadyNormalUserExist_ThrowsAlreadyExistsException() throws Exception {
+//        NormalUser normalUser = new NormalUser();
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        String jsonNormalUser = objectMapper.writeValueAsString(normalUser);
+//
+//        LOGGER.info("start");
+//        when(authService.register(normalUser)).thenThrow(AlreadyExistsException.class);
+//
+//        ResultActions response = mockMvc.perform(post("/api/auth/register-normal-user")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(jsonNormalUser));
+//
+//        response.andExpect(status().isConflict());
+//    }
+//
+//    @Order(3)
+//    @Test
+//    public void registerUser_AlreadyDriverUserExist_ThrowsAlreadyExistsException() throws Exception {
+//        DriverUser driverUser = new DriverUser();
+//        driverUser.setUserId(1L);
+//        driverUser.setName("Test User");
+//        driverUser.setUsername("test@gmail.com");
+//        driverUser.setPassword("12345678");
+//        driverUser.setMobNumber("1234567890");
+//        driverUser.setAddress("Abc, UP, India");
+//        driverUser.setCoinsEarned("1");
+//
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        String jsonNormalUser = objectMapper.writeValueAsString(driverUser);
+//
+//        when(authService.register(driverUser)).thenThrow(AlreadyExistsException.class);
+//
+//        ResultActions response = mockMvc.perform(post("/api/auth/register-driver-user")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(jsonNormalUser));
+//
+//        response.andExpect(status().isConflict());
+//    }
 }
